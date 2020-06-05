610625e114ef4c6b2612f9d78d581ecb544074f4
ypreddy9368 <y.pvan9368@gmail.com>
2020-06-05 14:49:14 +0530

    mvn

---
+++ src/test/java/com/icicitech/sampletest.java
@@ -0,0 +1,20 @@
+package com.icicitech;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class sampletest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
